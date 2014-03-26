8a5e9a685adf8822fda1a78295ab34fbdabffc24
Francisco Trindade <frank.trindade@gmail.com>
2014-03-26 13:04:47 +0000

    moved delivery options to taxons page

--- app/overrides/add_delivery_options_to_homepage.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
-                     :name => "add_delivery_options_to_homepage",
-                     :insert_top => "#wrapper",
-                     :partial => "spree/layouts/delivery_options",
-                     :disabled => false)
---
+++ app/overrides/add_delivery_options_to_taxon.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "spree/taxons/show",
+                     :name => "add_delivery_options_to_taxon",
+                     :insert_before => "h1.taxon-title",
+                     :partial => "spree/layouts/delivery_options",
+                     :disabled => false)
