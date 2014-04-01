5de62e37f26c60a71e20ae791bcdf25ca6be2a9a
Francisco Trindade <frank.trindade@gmail.com>
2014-04-01 16:30:24 +0100

    fixing styles for spree 2.2

---
+++ app/assets/javascripts/spree/frontend/spree_delivery_options.js
@@ -0,0 +1,4 @@
+//= require jquery
+//= require spree/frontend/delivery_time_options
+//= require spree/frontend/delivery_options_form
+//= require spree/frontend/jquery-ui-1.10.4.custom
--- app/assets/javascripts/store/spree_delivery_options.js
+++
@@ -1,4 +0,0 @@
-//= require jquery
-//= require store/delivery_time_options
-//= require store/delivery_options_form
-//= require store/jquery-ui-1.10.4.custom
