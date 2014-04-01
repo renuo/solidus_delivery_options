8f62dd877b79af7ba8369eecbaf5aafca507ddfb
Francisco Trindade <frank.trindade@gmail.com>
2014-04-01 17:26:35 +0100

    fixing overrides

--- app/overrides/add_delivery_options_link_order_page.rb
+++ app/overrides/add_delivery_options_link_order_page.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_order_submenu",
                      :name => "add_delivery_info_to_order_details",
                      :insert_bottom => "[data-hook='admin_order_tabs']",
                      :partial => "spree/admin/shared/delivery_options_link",
--- app/overrides/admin_order_information.rb
+++ app/overrides/admin_order_information.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_order_summary",
                      :name => "add_delivery_date_to_admin_order_information",
-                     :insert_after => "#order_total",
+                     :insert_top => "dl.additional-info",
                      :partial => "spree/admin/shared/admin_order_information_delivery_date",
                      :disabled => false)
