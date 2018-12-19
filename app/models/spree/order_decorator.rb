b77b7ee06c6b00bada121087c8d42e246e8664be
Lukas Bischof <lukas.bischof@renuo.ch>
2018-12-19 17:20:19 +0100

    Add localizations and custom templates

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -9,31 +9,31 @@ module Spree
 
     def valid_delivery_instructions?
       if delivery_instructions && delivery_instructions.length > 500
-        errors[:delivery_instructions] << 'cannot be longer than 500 charachters'
+        errors[:delivery_instructions] << I18n.t('activerecord.errors.models.spree/order.attributes.delivery_instructions.cannot_be_longer_than_500_charachters')
         return false
       end
       true
     end
 
     def delivery_date_present?
-      errors[:delivery_date] << 'cannot be blank' unless delivery_date
-      errors[:delivery_date].empty? ? true : false
+      errors[:delivery_date] << I18n.t('activerecord.errors.messages.blank') unless delivery_date
+      errors[:delivery_date].empty?
     end
 
     def delivery_time_present?
-      errors[:delivery_time] << 'cannot be blank' unless delivery_time
-      errors[:delivery_time].empty? ? true : false
+      errors[:delivery_time] << I18n.t('activerecord.errors.messages.blank') unless delivery_time
+      errors[:delivery_time].empty?
     end
 
     def valid_delivery_options?
       if (delivery_date && delivery_date_changed?) && (delivery_time && delivery_time_changed?)
-        errors[:delivery_date] << 'cannot be today or in the past' if delivery_date <= Date.current
+        errors[:delivery_date] << I18n.t('activerecord.errors.models.spree/order.attributes.delivery_date.cannot_be_today_or_in_the_past') if delivery_date <= Date.current
 
         options = current_delivery_options_for_date(delivery_date)
         if options.present?
-          errors[:delivery_time] << 'is invalid' unless options.include?(delivery_time)
+          errors[:delivery_time] << I18n.t('activerecord.errors.messages.invalid') unless options.include?(delivery_time)
         else
-          errors[:delivery_date] << 'is not available on the selected date.'
+          errors[:delivery_date] << I18n.t('activerecord.errors.models.spree/order.attributes.delivery_date.not_available_on_selected_date')
         end
       end
 
